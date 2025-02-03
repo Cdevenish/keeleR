a0008ff1014d010750b24c4a665cd24071bd1650
Christian Devenish <c.devenish@keele.ac.uk>
2025-02-03 19:18:48 +0000

    anova add

--- R/helpfiles.r
+++ R/helpfiles.r
@@ -9,6 +9,14 @@
 #' learnr::run_tutorial("A0_First_Steps_in_R", package = "keeleR")
 #' }
 NULL
+#' @name A1_Comparing_between_groups
+#' @title  A1 Comparing between groups
+#' @description
+#' An introduction to using R for t-tests and ANOVA, Wilcoxon and Kruskal-Wallis tests.
+#' @keywords R
+#' @examples
+#' learnr::run_tutorial("A1_Comparing_between_groups", package = "keeleR")
+NULL
 #' @name M1_Intro_to_multivariate
 #' @title Intro to multivariate data in R
 #' @description
